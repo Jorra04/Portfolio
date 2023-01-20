714a9d41f7bc63eb09e9c62db9d48b788e97fb90
Jorra <jorra04@my.yorku.ca>
2023-01-20 01:25:05 -0500

    feat: updated the code to move the header and footer to the _app component so it is on all pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,9 @@
-import Head from "next/head";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Jorra Singh Portfolio</title>
-        <meta
-          name="description"
-          content="A protfolio made by Jorra Singh to showcase my skills as a developer"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="h-screen px-10">
-        <section className="h-1/6">
-          <Navbar/>
-        </section>
-        <section className="h-4/6 flex flex-col justify-center">
-          <div className="text-center p-10">
+    <div className="text-center p-10">
             <h2 className=" text-5xl py-2 font-medium">
               Jorra Singh
             </h2>
@@ -38,12 +20,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </section>
-        <section className="h-1/6">
-          <Footer/>
-        </section>
-       
-      </main>
     </>
   );
 }
